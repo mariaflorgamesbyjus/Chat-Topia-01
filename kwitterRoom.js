cbebe2af38a228b2929f95d3e83a88281e5b35b0
May Gamer <119880616+mariaflorgamesbyjus@users.noreply.github.com>
2023-02-23 09:33:34 -0300

    Add files via upload

---
+++ kwitterRoom.js
@@ -0,0 +1,56 @@
+
+//ADICIONE SEUS LINKS FIREBASE
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjlgHa4XcWi-EGKIspJHJMQoLWrQ86xs0",
+  authDomain: "live-chat-teste.firebaseapp.com",
+  projectId: "live-chat-teste",
+  storageBucket: "live-chat-teste.appspot.com",
+  messagingSenderId: "966989881475",
+  appId: "1:966989881475:web:8494f13c7d14fc91cfbbb6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+  userName = localStorage.getItem("userName");
+
+document.getElementById("userName").innerHTML = "Bem-vindo(a) " + userName + "!";
+
+function addRoom()
+{
+  roomName = document.getElementById("roomName").value;
+
+  firebase.database().ref("/").child(roomName).update({
+    purpose : "adicionar nome de sala"
+  });
+
+    localStorage.setItem("roomName", roomName);
+    
+    window.location = "kwitterPage.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       roomNames = childKey;
+       console.log("Nome da Sala - " + roomNames);
+      row = "<div class='roomName' id="+roomNames+" onclick='redirectToRoomName(this.id)' >#"+ roomNames +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("roomName", name);
+    window.location = "kwitterPage.html";
+}
+
+function logout() {
+localStorage.removeItem("userName");
+localStorage.removeItem("roomName");
+    window.location = "index.html";
+}
